d9b40b9fc287de204ada218b84714af17ab31e77
Duc Viet <viet2k3nguyen@gmail.com>
2024-06-19 17:04:15 +0700

    them cach chay tren web

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -20,3 +20,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+// import 'package:device_preview/device_preview.dart';
+
+// void main() => runApp(
+//   DevicePreview(
+//     enabled: !kReleaseMode,
+//     builder: (context) => MyApp(), // Wrap your app
+//   ),
+// );
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       useInheritedMediaQuery: true,
+//       locale: DevicePreview.locale(context),
+//       builder: DevicePreview.appBuilder,
+//       theme: ThemeData.light(),
+//       darkTheme: ThemeData.dark(),
+//       home: const MainScreen(),
+//     );
+//   }
+// }
