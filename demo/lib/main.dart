42dd23aa7db976c3cf06e7bdedaebbae78ba28ba
Duc Viet <viet2k3nguyen@gmail.com>
2024-06-19 16:46:06 +0700

    sua giao dien

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:demo/screen/home_screen.dart';
+import 'package:demo/screen/main_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-    );
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   @override
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: HomeScreen(),
+      home: MainScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
